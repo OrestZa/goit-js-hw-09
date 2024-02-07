869d3ec08fdbde1ca128c30d994f4c873aecab93
OrestZa <119166956+OrestZa@users.noreply.github.com>
2024-02-07 03:12:48 +0100

    page settings

---
+++ src/js/1-gallery.js
@@ -0,0 +1,11 @@
+
+import simpleLightbox from 'simplelightbox';
+import SimpleLightbox from 'simplelightbox';
+
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  /* options */
+});
+
+console.log(simpleLightbox);
